33934d85870349065d750cad959cc07629355624
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-02-08 19:08:54 -0300

    Updates

---
+++ precache-manifest.297602cdf49a18bda2fc60de8736e5e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6eb224cab2eb0ed9f29edf3ee61745c",
+    "url": "/nao-oficial-project-movie-cards-library/index.html"
+  },
+  {
+    "revision": "c52522e99afd02fdd5cf",
+    "url": "/nao-oficial-project-movie-cards-library/static/css/main.f2916fa2.chunk.css"
+  },
+  {
+    "revision": "41a6e8268afb30a75857",
+    "url": "/nao-oficial-project-movie-cards-library/static/js/2.7593fc51.chunk.js"
+  },
+  {
+    "revision": "c52522e99afd02fdd5cf",
+    "url": "/nao-oficial-project-movie-cards-library/static/js/main.6717f2ed.chunk.js"
+  },
+  {
+    "revision": "fb77abde36bbe77a0cf6",
+    "url": "/nao-oficial-project-movie-cards-library/static/js/runtime-main.3feb0d76.js"
+  }
+]);
